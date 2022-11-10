057757598949915230183858742c47e686e568fe
Anais <victor.bolivar@pucp.edu.pe>
2022-11-09 20:21:38 -0500

    cree base dao

---
+++ src/main/java/com/example/lab9_base/Dao/BaseDao.java
@@ -0,0 +1,18 @@
+package com.example.lab9_base.Dao;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.DriverManager;
+
+public class BaseDao {
+    public Connection getConnection() throws SQLException {
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }catch (ClassNotFoundException e){
+            throw new RuntimeException(e);
+        }
+        String user = "root";
+        String pass = "123456";
+        String url = "jdbc:mysql://localhost:3306/lab9";
+        return DriverManager.getConnection(url, user, pass);
+    }
+}
