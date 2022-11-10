9fbbd94ce3fbe264a0cd46e05da65a3eb57f5393
AnaisSalvador <anaissalvadorrodriguez@gmail.com>
2022-11-10 08:54:40 -0500

    SELEC

--- src/main/java/com/example/lab9_base/Dao/DaoSelecciones.java
+++ src/main/java/com/example/lab9_base/Dao/DaoSelecciones.java
@@ -1,16 +1,37 @@
 package com.example.lab9_base.Dao;
 
 import com.example.lab9_base.Bean.Seleccion;
-
+import com.example.lab9_base.Bean.Estadio;
+import java.sql.Connection;
 import java.util.ArrayList;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
-public class DaoSelecciones {
+public class DaoSelecciones extends BaseDao{
     public ArrayList<Seleccion> listarSelecciones() {
 
         ArrayList<Seleccion> selecciones = new ArrayList<>();
-        /*
-        Inserte su código aquí
-        */
+        String sql = "SELECT * FROM seleccion";
+
+        try (Connection connection = this.getConnection();
+             Statement stmt = connection.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+
+            while (rs.next()) {
+                Seleccion seleccion = new Seleccion();
+                Estadio estadio = new Estadio();
+                seleccion.setIdSeleccion(rs.getInt(1));
+                seleccion.setNombre(rs.getString(2));
+                seleccion.setTecnico(rs.getString(3));
+                estadio.setIdEstadio(rs.getInt(4));
+                seleccion.setEstadio(estadio);
+                selecciones.add(seleccion);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return selecciones;
     }
 
