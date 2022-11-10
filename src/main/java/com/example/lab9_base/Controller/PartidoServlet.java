a6738823be69e277bcfead24928783d8102d54d4
AnaisSalvador <anaissalvadorrodriguez@gmail.com>
2022-11-10 07:48:39 -0500

    partido

--- src/main/java/com/example/lab9_base/Controller/PartidoServlet.java
+++ src/main/java/com/example/lab9_base/Controller/PartidoServlet.java
@@ -5,6 +5,10 @@ import com.example.lab9_base.Dao.DaoPartidos;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
+import com.example.lab9_base.Bean.Seleccion;
+import com.example.lab9_base.Dao.DaoSelecciones;
+import com.example.lab9_base.Bean.Arbitro;
+import com.example.lab9_base.Dao.DaoArbitros;
 
 import java.io.IOException;
 import java.sql.SQLException;
@@ -24,32 +28,30 @@ public class PartidoServlet extends HttpServlet {
         opciones.add("numeroJornada");
         DaoPartidos daoPartidos = new DaoPartidos();
         switch (action) {
-            case "buscar":
-                String buscarPar = request.getParameter("keyword");
-                ArrayList<Partido> listaFiltrada = daoPartidos.busqueda(buscarPar);
-
-                request.setAttribute("listaPartido", listaFiltrada);
-                view= request.getRequestDispatcher("/index.jsp");
-                view.forward(request, response);
-                break;
-
             case "guardar":
                 String jornadaSer = request.getParameter("numeroJornada");
                 int jonadaNum = Integer.parseInt(jornadaSer);
                 String fechaSer = request.getParameter("fecha");
                 String seleccionLocalSer = request.getParameter("seleccionLocal");
+                int seleccionLocalID = Integer.parseInt(seleccionLocalSer);
                 String seleccionVisitanteSer = request.getParameter("seleccionVisitante");
+                int selecionVisitaneID = Integer.parseInt(seleccionVisitanteSer);
                 String arbitro = request.getParameter("arbitro");
+                int arbitroSer = Integer.parseInt(arbitro);
 
                 try{
                     Partido partidoNew = new Partido();
-
+                    Seleccion seleccionNew = new Seleccion();
+                    Seleccion seleccionNeww = new Seleccion();
+                    Arbitro arbitro1 = new Arbitro();
                     partidoNew.setNumeroJornada(jonadaNum);
                     partidoNew.setFecha(fechaSer);
-                    partidoNew.setSeleccionLocal(seleccionLocalSer);
-                    partidoNew.setSeleccionVisitante(seleccionVisitanteSer);
-                    partidoNew.setArbitro(arbitro);
-
+                    seleccionNew.setIdSeleccion(seleccionLocalID);
+                    partidoNew.setSeleccionLocal(seleccionNew);
+                    seleccionNeww.setIdSeleccion(selecionVisitaneID);
+                    partidoNew.setSeleccionVisitante(seleccionNeww);
+                    arbitro1.setIdArbitro(arbitroSer);
+                    partidoNew.setArbitro(arbitro1);
                     daoPartidos.crearPartido(partidoNew);
                     response.sendRedirect(request.getContextPath()+"/PartidoServlet");
                 }catch (NumberFormatException e){
@@ -64,14 +66,21 @@ public class PartidoServlet extends HttpServlet {
 
         String action = request.getParameter("action") == null ? "lista" : request.getParameter("action");
         RequestDispatcher view;
+        DaoArbitros daoArbitros = new DaoArbitros();
+        DaoPartidos daoPartidos = new DaoPartidos();
+        DaoSelecciones daosSeleciones = new DaoSelecciones();
+        DaoSelecciones daoSeleccioness = new DaoSelecciones();
         switch (action) {
             case "lista":
-                request.setAttribute("Lista", action);
+                request.setAttribute("Lista", daoPartidos.listaDePartidos());
                 view = request.getRequestDispatcher("index.jsp");
                 view.forward(request, response);
                 break;
             case "crear":
-                view = request.getRequestDispatcher("/PartidoServlet?action=crear");
+                request.setAttribute("listaArbitro", daoArbitros.listarArbitros());
+                request.setAttribute("ListaSelecciones", daosSeleciones.listarSelecciones());
+                request.setAttribute("listaSleccioness", daoSeleccioness.listarSelecciones());
+                view = request.getRequestDispatcher("partidos/form.jps");
                 view.forward(request,response);
                 break;
         }
